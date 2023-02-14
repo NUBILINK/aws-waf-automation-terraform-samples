297d14c751605d1aa4839b433fe911f52ab19778
Juan R. Rodriguez <juanrr.ar@gmail.com>
2023-02-14 15:30:36 -0300

    New outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  description = "The ID of the WAF WebACL."
+  value       = aws_wafv2_web_acl.wafacl.id
+}
+
+output "arn" {
+  description = "The ARN of the WAF WebACL."
+  value       = aws_wafv2_web_acl.wafacl.arn
+}
