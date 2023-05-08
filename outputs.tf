c6c20f3335d87f7f7ebe674291704f6d4a9734c7
Juan R. Rodriguez <juanrr.ar@gmail.com>
2023-05-08 18:55:41 -0300

    Output webacl name

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "arn" {
   description = "The ARN of the WAF WebACL."
   value       = aws_wafv2_web_acl.wafacl.arn
 }
+
+output "name" {
+  description = "The Name of the WAF WebACL."
+  value       = aws_wafv2_web_acl.wafacl.name
+}
